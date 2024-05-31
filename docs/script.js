f14477fa45b58f3db1771ceaa8d070bcb2dc6c67
bbHUST <130190756+bbHUST@users.noreply.github.com>
2024-05-31 15:01:26 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,10 @@
 const button = document.getElementById('myButton');
 const message = document.getElementById('message');
+const body = document.body;
 
 button.addEventListener('click', () => {
-    message.textContent = 'Hey beautiful';
-    message.style.display = 'block';
+    body.style.backgroundColor = 'lightpink';
+    body.style.backgroundImage = 'url("https://static.vecteezy.com/system/resources/thumbnails/020/894/680/original/falling-rose-petals-in-4k-ultra-hd-rose-petals-for-valentine-and-wedding-background-free-video.jpg")';
     button.style.display = 'none';
+    message.style.display = 'block';
 });
